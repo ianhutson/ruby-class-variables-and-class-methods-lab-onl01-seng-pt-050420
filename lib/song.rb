d5f077a3c6177e0046482f4779fd750b6594137f
Ian Hutson <ih00209@georgiasouthern.edu>
2020-05-19 14:58:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,4 +36,15 @@ ef self.artist_count
 def self.genres
   @@genres.uniq
 end
+ef self.genre
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
 end
