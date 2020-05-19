f3adf459214a534f70c0a99bdff9e096071c3010
Ian Hutson <ih00209@georgiasouthern.edu>
2020-05-19 14:48:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,12 @@
 class Song
 attr_accessor :name, :genre, :artist  
 @@count = 0
-
+@@artist = []
 def initialize(name, genre, artist)
   @name = name
   @genre = genre
   @artist = artist
+  
   @@count += 1
 end
 
