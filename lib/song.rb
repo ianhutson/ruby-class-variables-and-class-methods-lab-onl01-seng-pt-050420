2f13f3a60a368cbf80bfd5c05f7a66a9d477c531
Ian Hutson <ih00209@georgiasouthern.edu>
2020-05-19 15:00:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ end
 
 def self.genre_count
     genre_count = {}
-    @@artists.each do |genre|
+    @@genres.each do |genre|
       if genre_count[genre]
         genre_count[genre] += 1 
       else
